9aab4c77edc0bd9a807322d5194d714d31256ddb
rodri720 <tomiyrami@gamil.com>
2023-12-27 00:50:45 -0300

    practica lista

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.export = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public/js',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
